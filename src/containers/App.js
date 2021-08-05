c6f6a0a45e64b40ca92d00befa2765de2a7ccbbb
Fabianm99 <fabianjose99@gmail.com>
2021-08-05 11:45:55 -0500

    map items ok

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Header from "../components/Header";
 import Search from "../components/Search";
@@ -6,43 +6,28 @@ import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
-
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img2.jpg";
-import img3 from "../assets/img3.jpg";
-import img4 from "../assets/img4.jpg";
-
 import useInitialState from "../Hooks/useInitialState";
 
 import "./styles/App.scss";
 
 const App = () => {
 
-  const [videos] = useInitialState('http://localhost:3002/initialState');
+  const [videos] = useInitialState("http://localhost:3002/initialState");
 
   return (
-
     <React.Fragment>
-
       <Header />
       <Search />
 
-      <Categories nombre="Mi lista">
-        <Carousel>
-          <CarouselItem url={img1}></CarouselItem>
-          <CarouselItem url={img2}></CarouselItem>
-          <CarouselItem url={img3}></CarouselItem>
-          <CarouselItem url={img4}></CarouselItem>
-        </Carousel>
-      </Categories>
-
-      <Categories nombre="Tendencias">
-        <Carousel>
-          {videos.trends && videos.trends.map( video =>
-          <CarouselItem {...video} ></CarouselItem>
-          )}
-        </Carousel>
-      </Categories>
+      {Object.keys(videos).map( name => (
+        <Categories nombre={name}>
+          <Carousel>
+            {videos[name].map( videos => (
+              <CarouselItem {...videos}></CarouselItem>
+            ))}
+          </Carousel>
+        </Categories>
+      ))}
 
       <Footer />
     </React.Fragment>
